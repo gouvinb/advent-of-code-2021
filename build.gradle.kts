a303d79a085484d3fd6691052c684e0575eb5d33
bruno-gouvinhas <bruno.gouvinhas@appscho.com>
2021-12-02 12:23:13 +0100

    add detekt plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.0"
     application
+
+    id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
 
 group = "app.gouvinb"
@@ -26,4 +29,27 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<Detekt> {
+    reports {
+        html.required.set(true)
+        html.outputLocation.set(file("build/reports/detekt.html"))
+
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+    }
+}
+
+detekt {
+    source = files("src/main/java", "src/test/java", "src/main/kotlin", "src/test/kotlin")
+    parallel = true
+
+    // Define the detekt configuration(s) you want to use.
+    // Defaults to the default detekt configuration.
+    // config = files("path/to/config.yml")
+
+    // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+    // baseline = file("path/to/baseline.xml")
 }
